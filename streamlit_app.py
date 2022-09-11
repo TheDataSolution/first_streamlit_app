2944c7add2d7a489742d6a22f7a32b2e1b5d797b
TheDataSolution <112708822+TheDataSolution@users.noreply.github.com>
2022-09-11 23:19:20 +0200

    Update streamlit_app.py
    Normalized Json format in table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
+
+# take the json version and normalized it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output the screen in a table
+streamlit.dataframe(fruityvice_normalized)
